3ac54ecb16f9e71539efcd43deacab49b0d5d1a4
ChrisWess <christoph_wessarges@hotmail.com>
2022-06-10 16:55:03 +0200

    Changed general design and added some ui elements for the coreference viewer

--- coref-app/src/Components/CorefView.tsx
+++ coref-app/src/Components/CorefView.tsx
@@ -1,12 +1,26 @@
 import React, {useState} from 'react';
 import { useTheme } from '@mui/material/styles';
+import {Button, Box, FormControlLabel, Checkbox} from "@mui/material";
+import MyList from "./MyList";
 
 export default function CorefView() {
     const theme = useTheme();
 
+    // TODO: make the text (in Box) have the same color as the mention cluster that it is from
+    // TODO: dynamically add more list entries to MyList, when coreference is selected
     return (
         <>
-            <p>Here you can see the corefs</p>
+            <p>Current Text Selection:</p>
+            <Box sx={{ backgroundColor: "lightgray", border: 1, borderColor: 'black', borderRadius: 1 }}>
+                <p style={{textAlign: "center"}}>No Selection</p>
+            </Box>
+            <p>Coreferences:</p>
+            <MyList/>
+
+            <Button variant="outlined" style={{margin: 5}}>Add new Coreference</Button>
+            <Button variant="outlined" style={{margin: 5}}>Delete Coreference</Button>
+
+            <FormControlLabel control={<Checkbox defaultChecked />} style={{margin: 5}} label="Auto-Annotate" />
         </>
     );
 }
