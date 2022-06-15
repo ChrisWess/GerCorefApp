bcff5e49a4fd4317eb93ac2d847c16af58d72a30
ChrisWess <christoph_wessarges@hotmail.com>
2022-06-16 01:34:38 +0200

    added coloring to text selection

--- coref-app/src/Components/CorefView.tsx
+++ coref-app/src/Components/CorefView.tsx
@@ -6,20 +6,23 @@ import MyList from "./MyList";
 interface CorefViewProps {
     selectedCoref: number[],
     wordArr: MutableRefObject<string[]>
+    clusterColor: string
     handleSelectCoref: Function;
 }
 
-const CorefView: React.FC<CorefViewProps> = ({ selectedCoref, wordArr, handleSelectCoref }) => {
+const CorefView: React.FC<CorefViewProps> = ({ selectedCoref, wordArr,
+                                                 clusterColor, handleSelectCoref }) => {
     const theme = useTheme();
 
-    // TODO: make the text (in Box) have the same color as the mention cluster that it is from
     // TODO: dynamically add more list entries to MyList, when coreference is selected
     return (
         <>
             <p>Current Text Selection:</p>
-            <Box sx={{ backgroundColor: "lightgray", border: 1, borderColor: 'black', borderRadius: 1 }}>
-                <p style={{textAlign: "center"}}>{selectedCoref.length == 0 ? "No Selection" :
-                    wordArr.current.slice(selectedCoref[0], selectedCoref[1]).join(" ")}</p>
+            <Box sx={{ backgroundColor: "gray", border: 1, borderColor: 'black', borderRadius: 1 }}>
+                <b><p style={{textAlign: "center", color: clusterColor, fontSize: "18px"}}>
+                    {selectedCoref.length == 0 ? "No Selection" :
+                        wordArr.current.slice(selectedCoref[0], selectedCoref[1]).join(" ")}
+                </p></b>
             </Box>
             <p>Coreferences:</p>
             <MyList handleSelectCoref={handleSelectCoref}/>
