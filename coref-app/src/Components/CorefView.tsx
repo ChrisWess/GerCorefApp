df2f94f41eb03b7b6a70dc7932cffd7f4e85d138
ChrisWess <christoph_wessarges@hotmail.com>
2022-06-22 18:13:25 +0200

    adding coreferences works internally

--- coref-app/src/Components/CorefView.tsx
+++ coref-app/src/Components/CorefView.tsx
@@ -1,95 +1,32 @@
-import React, {MutableRefObject, useState} from 'react';
+import React, {MutableRefObject} from 'react';
 import { useTheme } from '@mui/material/styles';
 import {Button, Box, FormControlLabel, Switch} from "@mui/material";
 import MyList from "./MyList";
-import {Mention} from "./MainView";
+import {Mention, parseMentionId} from "./MainView";
 import AddCoreference from "./AddCoreference";
 
 interface CorefViewProps {
     selectedCoref: number[],
     wordArr: MutableRefObject<string[]>
-    allCorefsMapped: MutableRefObject<Map<string, Mention>>
+    corefClusters: number[][][]
     allCorefs: MutableRefObject<Mention[][]>
     clusterColor: string
     markedWord: MutableRefObject<number[]>
     currentMention: Mention | undefined
     handleSelectCoref: Function;
     setCurrentMention: Function
+    setCorefClusters: Function
 }
 
-export const parseMentionId = function(mentionId: string) {
-    let docIdx: number = 1
-    let clusterIdx: number = mentionId.indexOf("c")
-    let mentionIdx: number = mentionId.indexOf("m")
-    docIdx = parseInt(mentionId.substring(docIdx, clusterIdx))
-    clusterIdx = parseInt(mentionId.substring(clusterIdx + 1, mentionIdx))
-    mentionIdx = parseInt(mentionId.substring(mentionIdx + 1))
-    return {docIdx: docIdx, clusterIdx: clusterIdx, mentionIdx: mentionIdx}
-};
+export const getCluster = function(mention: Mention, allCorefs: Mention[][]) {
+    let mentionLoc = parseMentionId(mention.id)
+    return allCorefs[mentionLoc.clusterIdx]
+}
 
-export const addNewCoref = function(clusterId: number, allCorefs: MutableRefObject<Mention[][]>,
-                                    wordArr: string[], markedWord: number[]) {
-    console.log(markedWord.length)
-    if (markedWord.length > 0) {
-        if (clusterId > allCorefs.current.length) {
-            allCorefs.current.push([])
-        }
-        let clusterIdx: number = clusterId - 1
-        let corefId = `d1c${clusterIdx}m${allCorefs.current[clusterIdx].length}`
-        let mention: Mention
-        let elem
-        let replacement
-        let idxStart: number = markedWord[0]
-        if (markedWord.length === 1) {
-            elem = document.getElementById("w" + idxStart)
-            replacement = document.createElement('b');
-            replacement.innerHTML = "<b id=\"" + corefId + "\" class=\"cr cr-" + clusterId +
-                      `"><bb id="w${idxStart}"> <a id="w${idxStart}" href="#d1c1m1">[</a>` +
-                        wordArr[idxStart] + `<a id="w${idxStart}" href="#d1c1m1">]</a><sub id="w${idxStart}">` +
-                        clusterId + "</sub></bb></b>";
-            elem!.replaceWith(replacement)
-            mention = { id: corefId, content: wordArr[idxStart], selectionRange: [idxStart, idxStart + 1] }
-        } else {
-            elem = document.getElementById("w" + idxStart)!
-            elem.remove()
-
-            let newHTML: string = "<b id=\"" + corefId + "\" class=\"cr cr-" + clusterId +
-                       `"><bb id="w${idxStart}"> <a id="w${idxStart}" href="#d1c1m1">[</a>` +
-                        wordArr[idxStart] + "</bb>"
-
-            let idxEnd: number = markedWord[1] - 1
-            for (let i = idxStart + 1; i < idxEnd; i++) {
-                elem = document.getElementById("w" + i)!
-                elem.remove()
-                newHTML += `<bb id='w${i}'> ` + wordArr[i] + "</bb>";
-            }
-
-            elem = document.getElementById("w" + idxEnd)!
-            replacement = document.createElement('b');
-            newHTML += `<bb id="w${idxEnd}"> ` + wordArr[idxEnd] +
-                      `<a id="w${idxEnd}" href="#d1c1m1">]</a><sub id="w${idxEnd}">` +
-                        clusterId + "</sub></bb></b>";
-            replacement.innerHTML = newHTML
-            elem.replaceWith(replacement)
-            mention = { id: corefId, content: wordArr.slice(idxStart, markedWord[1]).join(" "), selectionRange: markedWord }
-        }
-        console.log(mention)
-        allCorefs.current[clusterIdx].push(mention)
-        return mention
-    }
-};
-
-const CorefView: React.FC<CorefViewProps> = ({ selectedCoref, wordArr, allCorefsMapped,
+const CorefView: React.FC<CorefViewProps> = ({ selectedCoref, wordArr, corefClusters,
                                                  allCorefs, clusterColor, markedWord,
-                                                 currentMention, handleSelectCoref, setCurrentMention }) => {
+                                                 currentMention, handleSelectCoref, setCurrentMention, setCorefClusters }) => {
     const theme = useTheme();
-
-    const currentCluster = React.useRef<Mention[]>([]);
-
-    if (currentMention) {
-        let mentionLoc = parseMentionId(currentMention.id)
-        currentCluster.current = allCorefs.current[mentionLoc.clusterIdx]
-    }
 
     // TODO: top button: open a popup window that lets you select the cluster that you want to assign the selection (marked word)
     //  as a new coreference to
@@ -106,7 +43,6 @@ const CorefView: React.FC<CorefViewProps> = ({ selectedCoref, wordArr, allCorefs
             <p>Coreferences:</p>
             <MyList
                 currentMention={currentMention}
-                currentCluster={currentCluster}
                 allCorefs={allCorefs}
                 markedWord={markedWord}
                 handleSelectCoref={handleSelectCoref}
@@ -115,12 +51,13 @@ const CorefView: React.FC<CorefViewProps> = ({ selectedCoref, wordArr, allCorefs
             <AddCoreference
                 selectedCoref={selectedCoref}
                 currentMention={currentMention}
+                corefClusters={corefClusters}
                 wordArr={wordArr}
-                allCorefsMapped={allCorefsMapped}
                 allCorefs={allCorefs}
                 markedWord={markedWord}
                 handleSelectCoref={handleSelectCoref}
-                setCurrentMention={setCurrentMention}/>
+                setCurrentMention={setCurrentMention}
+                setCorefClusters={setCorefClusters}/>
             <Button variant="outlined" style={{margin: 5, textTransform: "none", width: "97%"}} disabled={!currentMention}>
                 Delete Coreference
             </Button>
